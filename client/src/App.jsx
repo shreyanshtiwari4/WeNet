de6b87932cfda8ba437f0d01c1d40cbce62642c5
Shreyansh Tiwari <shreyansharipur@gmail.com>
2025-05-30 02:45:18 +0530

    signUp signIn page created and routes added for the same

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,28 @@
-function App() {
+import { lazy, Suspense } from "react";
+import { useSelector } from "react-redux";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { publicRoutes } from "./routes";
+
+import SignIn from "./pages/SignIn";
+
+const App = () => {
+  const userData = useSelector((state) => state.auth?.userData);
 
   return (
-    <>
-      <div className="text-[40px] B">Hello guys folder setup hogaya hai</div>
-    </>
-  )
-}
+    <Suspense fallback={<FallbackLoading />}>
+      <Routes>
 
-export default App
+        {publicRoutes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+
+        <Route
+          path="/signin"
+          element={userData ? <Navigate to="/" /> : <SignIn />}
+        />
+      </Routes>
+    </Suspense>
+  );
+};
+
+export default App;
---
+++ client/src/routes.js
@@ -0,0 +1,10 @@
+import { lazy } from "react";
+
+import SignUp from "./pages/SignUp";
+
+export const publicRoutes = [
+  {
+    path: "/signup",
+    element: <SignUp />,
+  }
+];
