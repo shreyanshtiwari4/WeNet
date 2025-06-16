46f53c480ce11595a9025d2bbe385ccaf5ea628c
shivdutt0411 <tiwari041102@gmail.com>
2025-06-16 23:14:02 +0530

    Added basic database connection setup

--- server/app.js
+++ server/app.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const {connectDB} = require('./config/db.js');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
